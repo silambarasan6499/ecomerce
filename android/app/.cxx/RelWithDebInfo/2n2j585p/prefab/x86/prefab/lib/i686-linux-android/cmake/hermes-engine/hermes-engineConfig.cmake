ac952e379492d89fd31d859ff0069d90ec853f20
silambarasan <silambarasan.m@optisolbusiness.com>
2023-07-13 09:51:43 +0530

    file update

---
+++ android/app/.cxx/RelWithDebInfo/2n2j585p/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/mac-obs-39/.gradle/caches/transforms-3/e1925d27dfeda554a62fa91e748dc57a/transformed/jetified-hermes-android-0.72.1-release/prefab/modules/libhermes/libs/android.x86/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/mac-obs-39/.gradle/caches/transforms-3/e1925d27dfeda554a62fa91e748dc57a/transformed/jetified-hermes-android-0.72.1-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
